19d0ab8cbd01efe872b2ac36ee24d26044ed3434
miran <miranararat722@gmail.com>
2025-01-18 10:12:54 -0500

    more work on authentication for the frontend

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Home";
 import Navbar from "./components/Navbar"
+import AuthProvider from "./hooks/authProvider";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 
 function App() {
   return (
     <Router>
+      <AuthProvider>
         <Navbar />
         <Routes>
           <Route path="/" element={<Homepage/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
           </Routes>
+      </AuthProvider>
     </Router>
   );
 }
